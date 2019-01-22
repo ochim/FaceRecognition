3eabfb481db89d7146cd1216d75c2f4d8eaec0e6
ochi <mune.ochi@gmail.com>
2019-01-22 18:21:49 +0900

    ボタンのアクションを書き換え

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -9,17 +9,102 @@
 import UIKit
 import Vision
 import CoreImage
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     private let originalImage = UIImage(named: "futago")
 
+    @IBOutlet weak var faceDetectButton: UIButton!
+    @IBOutlet weak var fillFaceButton: UIButton!
+    @IBOutlet weak var mozaikuButton: UIButton!
+    @IBOutlet weak var faceMozaikuButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageView.image = originalImage
+        
+        faceDetectButton.rx.tap.subscribe(onNext: { _ in
+            let request = VNDetectFaceRectanglesRequest { (request, error) in
+                var image = self.originalImage
+                for observation in request.results as! [VNFaceObservation] {
+                    image = self.drawFaceRectangle(image: image, observation: observation)
+                }
+                
+                self.imageView.image = image
+            }
+            
+            if let cgImage = self.originalImage?.cgImage {
+                let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+                try? handler.perform([request])
+            }
+
+        }).disposed(by: disposeBag)
+        
+        fillFaceButton.rx.tap.subscribe(onNext: { _ in
+            let request = VNDetectFaceRectanglesRequest { (request, error) in
+                
+                var image = self.originalImage!
+                for observation in request.results as! [VNFaceObservation] {
+                    let rect = observation.boundingBox.converted(to: image.size)
+                    
+                    UIGraphicsBeginImageContextWithOptions(image.size, false, 0.0)
+                    let context = UIGraphicsGetCurrentContext()
+                    image.draw(in: CGRect(origin: .zero, size: image.size))
+                    context?.setFillColor(UIColor.black.cgColor)
+                    context?.fill(rect)
+                    let drawnImage = UIGraphicsGetImageFromCurrentImageContext()
+                    UIGraphicsEndImageContext()
+                    
+                    image = drawnImage!
+                }
+                self.imageView.image = image
+            }
+            
+            if let cgImage = self.originalImage?.cgImage {
+                let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+                try? handler.perform([request])
+            }
+
+        }).disposed(by: disposeBag)
+        
+        mozaikuButton.rx.tap.subscribe(onNext: { _ in
+            self.imageView.image = self.mozaiku(image: self.originalImage!, block: 10)
+        }).disposed(by: disposeBag)
+        
+        faceMozaikuButton.rx.tap.subscribe(onNext: { _ in
+            let request = VNDetectFaceRectanglesRequest { (request, error) in
+                let image = self.originalImage!
+                let mi = self.mozaiku(image: image, block: 10)
+                
+                var tmp: UIImage? = nil
+                for observation in request.results as! [VNFaceObservation] {
+                    let rect = observation.boundingBox.converted(to: image.size)
+                    let ci = mi.cropping(to: rect)
+                    if tmp == nil {
+                        tmp = image.composite(image: ci!, imageX: rect.origin.x, imageY: rect.origin.y)
+                    } else {
+                        tmp = tmp!.composite(image: ci!, imageX: rect.origin.x, imageY: rect.origin.y)
+                    }
+                }
+                self.imageView.image = tmp
+            }
+            
+            if let cgImage = self.originalImage?.cgImage {
+                let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+                try? handler.perform([request])
+            }
+
+        }).disposed(by: disposeBag)
+        
+        resetButton.rx.tap.subscribe(onNext: { _ in
+            self.imageView.image = self.originalImage
+        }).disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,51 +112,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func reset(_ sender: Any) {
-        self.imageView.image = originalImage
-    }
-    
-    // モザイク
-    @IBAction func filterImage(_ sender: Any) {
-        self.imageView.image = mozaiku(image: originalImage!, block: 10)
-    }
-    
     private func mozaiku(image: UIImage, block: CGFloat) -> UIImage {
         let ciPhoto = CIImage(cgImage: image.cgImage!)
         
         // フィルタの名前を指定する(モザイク処理)
         let filter = CIFilter(name: "CIPixellate")
-        // setValueで対象の画像、効果を指定する
-        filter?.setValue(ciPhoto, forKey: kCIInputImageKey) // フィルタをかける対象の写真
-        filter?.setValue(block, forKey: "inputScale")          // ブロックの大きさ
+        filter?.setValue(ciPhoto, forKey: kCIInputImageKey)
+        filter?.setValue(block, forKey: "inputScale")      // ブロックの大きさ
         
-        // フィルタ処理のオブジェクト
         let filteredImage:CIImage = (filter?.outputImage)!
-        // 矩形情報をセットしてレンダリング
         let ciContext:CIContext = CIContext(options: nil)
         let imageRef = ciContext.createCGImage(filteredImage, from: filteredImage.extent)
         
         // scaleに注意
         let outputImage = UIImage(cgImage:imageRef!, scale:UIScreen.main.scale, orientation:UIImageOrientation.up)
         return outputImage
-    }
-    
-    // 顔検出
-    @IBAction func faceDetection() {
-        
-        let request = VNDetectFaceRectanglesRequest { (request, error) in
-            var image = self.originalImage
-            for observation in request.results as! [VNFaceObservation] {
-                image = self.drawFaceRectangle(image: image, observation: observation)
-            }
-            
-            self.imageView.image = image
-        }
-        
-        if let cgImage = self.originalImage?.cgImage {
-            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-            try? handler.perform([request])
-        }
     }
     
     private func drawFaceRectangle(image: UIImage?, observation: VNFaceObservation) -> UIImage? {
@@ -89,61 +144,6 @@ class ViewController: UIViewController {
         return drawnImage
     }
     
-    // 顔塗りつぶし
-    @IBAction func fillFace() {
-        
-        let request = VNDetectFaceRectanglesRequest { (request, error) in
-            
-            var image = self.originalImage!
-            for observation in request.results as! [VNFaceObservation] {
-                let rect = observation.boundingBox.converted(to: image.size)
-
-                UIGraphicsBeginImageContextWithOptions(image.size, false, 0.0)
-                let context = UIGraphicsGetCurrentContext()
-                image.draw(in: CGRect(origin: .zero, size: image.size))
-                context?.setFillColor(UIColor.black.cgColor)
-                context?.fill(rect)
-                let drawnImage = UIGraphicsGetImageFromCurrentImageContext()
-                UIGraphicsEndImageContext()
-
-                image = drawnImage!
-            }
-            self.imageView.image = image
-        }
-        
-        if let cgImage = self.originalImage?.cgImage {
-            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-            try? handler.perform([request])
-        }
-        
-    }
-    
-    // 顔モザイク
-    @IBAction func faceMozaiku() {
-
-        let request = VNDetectFaceRectanglesRequest { (request, error) in
-            let image = self.originalImage!
-            let mi = self.mozaiku(image: image, block: 10)
-            
-            var tmp: UIImage? = nil
-            for observation in request.results as! [VNFaceObservation] {
-                let rect = observation.boundingBox.converted(to: image.size)
-                let ci = mi.cropping(to: rect)
-                if tmp == nil {
-                    tmp = image.composite(image: ci!, imageX: rect.origin.x, imageY: rect.origin.y)
-                } else {
-                    tmp = tmp!.composite(image: ci!, imageX: rect.origin.x, imageY: rect.origin.y)
-                }
-            }
-            self.imageView.image = tmp
-        }
-        
-        if let cgImage = self.originalImage?.cgImage {
-            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-            try? handler.perform([request])
-        }
-
-    }
 }
 
 extension CGRect {
