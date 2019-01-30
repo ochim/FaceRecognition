e84e4107607b55fdd9bddf2a34060da3ef3789d3
ochi <mune.ochi@gmail.com>
2019-01-30 15:58:28 +0900

    UIImagePickerControllerの起動と選択した画像の処理周りをRxで書き直す

--- FaceRecognition/AppDelegate.swift
+++ FaceRecognition/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // for ImagePicker
+        RxImagePickerDelegateProxy.register { RxImagePickerDelegateProxy(imagePicker: $0) }
+
         return true
     }
 
